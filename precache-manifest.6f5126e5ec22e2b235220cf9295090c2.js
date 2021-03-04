13fd555113b4877633eb6962ac3940ae792caf7b
fatih durna <fdfatihdurna@gmail.com>
2021-03-04 23:28:48 +0300

    Updates

---
+++ precache-manifest.6f5126e5ec22e2b235220cf9295090c2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "badb65e8e2c0d565a9ff8d426fe2f1de",
+    "url": "/react-slack-clone/index.html"
+  },
+  {
+    "revision": "c1eeff6e1fe5ef553867",
+    "url": "/react-slack-clone/static/css/2.1d16949a.chunk.css"
+  },
+  {
+    "revision": "2dbb91fade424dee73c2",
+    "url": "/react-slack-clone/static/css/main.7a5cf0c8.chunk.css"
+  },
+  {
+    "revision": "c1eeff6e1fe5ef553867",
+    "url": "/react-slack-clone/static/js/2.570eb8b0.chunk.js"
+  },
+  {
+    "revision": "fb38d244761f3aee0c2a13e6bc129471",
+    "url": "/react-slack-clone/static/js/2.570eb8b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dbb91fade424dee73c2",
+    "url": "/react-slack-clone/static/js/main.3f2bdcf3.chunk.js"
+  },
+  {
+    "revision": "2ca4ea197569968b9ff0",
+    "url": "/react-slack-clone/static/js/runtime-main.d9c7c478.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-slack-clone/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-slack-clone/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react-slack-clone/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react-slack-clone/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-slack-clone/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react-slack-clone/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react-slack-clone/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react-slack-clone/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react-slack-clone/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react-slack-clone/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-slack-clone/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react-slack-clone/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-slack-clone/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-slack-clone/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react-slack-clone/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react-slack-clone/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
